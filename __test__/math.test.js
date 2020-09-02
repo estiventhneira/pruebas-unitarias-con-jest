ed3bc3792cff82f982846311391ca34c2d8d8f61
Estiventh Neira <estiventhneira@gmail.com>
2020-09-01 23:33:33 -0500

    aliminando npx de mis scripts (mejora el rendimiento)

--- __test__/math.test.js
+++ __test__/math.test.js
@@ -7,4 +7,11 @@ describe("calculos matematicos", () => {
   test("prueba de multiplicar", () => {
     expect(multiplicar(2, 4)).toBe(8);
   });
+  test("Pruebas con Restas", () => {
+    expect(restar(1, 1)).toBe(0);
+  });
+
+  test("Pruebas con Divisiones", () => {
+    expect(dividir(10, 5)).toBe(5);
+  });
 });
