90052c69e53fc881017f104622df5d4c517cfea9
Estiventh Neira <estiventhneira@gmail.com>
2020-09-01 23:29:12 -0500

    primeras pruebas

---
+++ __test__/math.test.js
@@ -0,0 +1,10 @@
+import { sumar, multiplicar, restar, dividir } from "../maths";
+
+describe("calculos matematicos", () => {
+  test("Pruebas con Sumas", () => {
+    expect(sumar(1, 1)).toBe(2);
+  });
+  test("prueba de multiplicar", () => {
+    expect(multiplicar(2, 4)).toBe(8);
+  });
+});
